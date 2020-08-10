3060da54ca7caab0917ddd8fc2e59678255527a8
Aditya Nakadi <nakadiaditya@gmail.com>
2020-08-10 22:54:34 +0530

    Updates

---
+++ precache-manifest.b1740ec5fe0303a4d4934894bfb1d8ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e4fca27e24fd75b91c3c54fb51dde7",
+    "url": "/vidly-react/index.html"
+  },
+  {
+    "revision": "1a8569a850670a7efb22",
+    "url": "/vidly-react/static/css/2.02a6896a.chunk.css"
+  },
+  {
+    "revision": "fda361df3b9a5a1798a5",
+    "url": "/vidly-react/static/css/main.4875e98a.chunk.css"
+  },
+  {
+    "revision": "1a8569a850670a7efb22",
+    "url": "/vidly-react/static/js/2.6359d05c.chunk.js"
+  },
+  {
+    "revision": "4c796ab9273dad0140c4d6224d61af40",
+    "url": "/vidly-react/static/js/2.6359d05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda361df3b9a5a1798a5",
+    "url": "/vidly-react/static/js/main.3adb6e56.chunk.js"
+  },
+  {
+    "revision": "3a64074761575fbef69d",
+    "url": "/vidly-react/static/js/runtime-main.3ea62532.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
